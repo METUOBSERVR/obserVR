01ffe3a594e9b2702454c78bf8cacc7ace432c79
obserVR <company.observr@gmail.com>
2024-11-19 23:17:20 +0300

    Basic chorono introduction and structure is added.

---
+++ SENSOR/SOURCE/TIMER_IN_CPP/README.md
@@ -0,0 +1,5 @@
+# HOW TO RUN
+
+
+## RUN MAKE FILE
+-   "mingw32-make" for windows   
--- SENSOR/SOURCE/TIMER_IN_CPP/SRC/main.cpp
+++ SENSOR/SOURCE/TIMER_IN_CPP/SRC/main.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
-
-
+#include "counter.hpp"
+#include <thread>
 
 
 
 
 
 int main () {
-    std::cout << "Mete"<<std::endl;
+    counter timer;
 
 
+    timer.update_time_now_for_system_clock();
+    timer.print_time_now_for_system_clock();
+    std::this_thread::sleep_for(std::chrono::microseconds(200));
+    timer.update_time_now_for_system_clock();
+    timer.print_time_now_for_system_clock();
+; 
 
 
+    timer.update_time_now_for_high_resolution_clock();
+    timer.print_time_now_for_high_resolution_clock();
+    std::this_thread::sleep_for(std::chrono::microseconds(200));
+    timer.update_time_now_for_high_resolution_clock();
+    timer.print_time_now_for_high_resolution_clock();
 
 
 
