1a07990b5c32b463ce3c91b5946a7aa504eb3e77
METUOBSERVR <company.observr@gmail.com>
2024-11-21 16:46:03 +0300

    BASIC DEVICE CONNECTION SOFTWARE DEVELOPMENT IS SET

--- SENSOR/SOURCE/SOFTWARE/BASIC_COMMUNICATION/SRC/main.cpp
+++ SENSOR/SOURCE/SOFTWARE/BASIC_COMMUNICATION/SRC/main.cpp
@@ -9,10 +9,17 @@
 int main () 
 {
     uint8_t data_return;
+    char    control;
     I2Cdev  ic2_imu;
     ic2_imu.initialize();
     ic2.enable()
-    ic2.readByte(0x68, 0x75, &data_return);
-    std::cout << "DEVICE ID:" << (int)data_return << std::endl;
+    while (true)
+    {
+        ic2.readByte(0x68, 0x75, &data_return);
+        std::cout << "DEVICE ID:" << (int)data_return << std::endl;
+        control = getchar();
+        if (control == "q") break
+    };
+    std::cout << "DEVICE CONNECTION IS TURNED OFF"
     return 0;
 }
