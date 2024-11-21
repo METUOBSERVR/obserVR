08082f1c8a223dadd7882aed48422df811e0975c
METUOBSERVR <company.observr@gmail.com>
2024-11-21 16:15:22 +0300

    The basic i2c interface is installed and configurated.

--- SENSOR/SOURCE/SOFTWARE/BASIC_COMMUNICATION/README.md
+++ SENSOR/SOURCE/SOFTWARE/BASIC_COMMUNICATION/README.md
@@ -1,5 +1,17 @@
-# HOW TO RUN
+## HOW TO RUN
+1. Update apt get with "sudo apt update" (Linux)
+2. Upgrade apt get with "sudo apt upgrade" (Linux)
+3. Get bcm2835 package with "wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.xx.tar.gz" (current directory so do not download it into porject directory)
+4. Unpackage bcm2835 with "tar zxvf bcm2835-1.xx.tar.gz" (Check lastest version)
+5. Go to bcm2835 directory with "cd bcm2835-1.xx"
+6. Run the configuration with "./configure"
+7. Run make file with "make"
+8. Run make install with "sudo make install"
+9. Compile code and check whether it is install correctly or not 
+10. Make the output file executable with "chmod +x -file name-"
+
 
 
 ## RUN MAKE FILE
 -   "mingw32-make" for windows   
+-   "make" for linux
--- SENSOR/SOURCE/SOFTWARE/BASIC_COMMUNICATION/SRC/main.cpp
+++ SENSOR/SOURCE/SOFTWARE/BASIC_COMMUNICATION/SRC/main.cpp
@@ -1,31 +1,13 @@
-#include <iostream>
-#include "counter.hpp"
-#include <thread>
+#include <iostream>     // CPP STANDARD LIBRARY INSERTION 
+#include <stdio.h>      // C STANDARD LIBRARY INSERTION
+#include "i2c.hpp"
 
 
 
 
 
-int main () {
-    counter timer;
-
-
-    timer.update_time_now_for_system_clock();
-    timer.print_time_now_for_system_clock();
-    std::this_thread::sleep_for(std::chrono::microseconds(200));
-    timer.update_time_now_for_system_clock();
-    timer.print_time_now_for_system_clock();
-; 
-
-
-    timer.update_time_now_for_high_resolution_clock();
-    timer.print_time_now_for_high_resolution_clock();
-    std::this_thread::sleep_for(std::chrono::microseconds(200));
-    timer.update_time_now_for_high_resolution_clock();
-    timer.print_time_now_for_high_resolution_clock();
-
-
-
-
+int main () 
+{
+    I2Cdev  ic2_imu;
     return 0;
 }
