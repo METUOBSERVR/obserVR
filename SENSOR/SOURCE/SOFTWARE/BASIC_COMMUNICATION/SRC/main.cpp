f23d30c1552ac9047f1daf51731875f617db09d2
METUOBSERVR <company.observr@gmail.com>
2024-11-21 16:42:31 +0300

    BASIC DEVICE CONNECTION SOFTWARE DEVELOPMENT IS SET

--- SENSOR/SOURCE/SOFTWARE/BASIC_COMMUNICATION/SRC/main.cpp
+++ SENSOR/SOURCE/SOFTWARE/BASIC_COMMUNICATION/SRC/main.cpp
@@ -8,6 +8,11 @@
 
 int main () 
 {
+    uint8_t data_return;
     I2Cdev  ic2_imu;
+    ic2_imu.initialize();
+    ic2.enable()
+    ic2.readByte(0x68, 0x75, &data_return);
+    std::cout << "DEVICE ID:" << (int)data_return << std::endl;
     return 0;
 }
